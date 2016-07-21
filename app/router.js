66cc9c36d75f0a479e9a317abd676ce16e2d261f
V4Vince <vince.guoo@gmail.com>
2016-07-21 15:27:05 -0400

    Adds profile route

--- app/router.js
+++ app/router.js
@@ -9,7 +9,7 @@ Router.map(function () {
   this.route('sign-up');
   this.route('sign-in');
   this.route('change-password');
-  this.route('users');
+  this.route('profiles');
 });
 
 export default Router;
